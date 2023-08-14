85ff655e39e42378a9f4982d4aac9b9a0c8a40bc
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-08-14 15:13:31 +0300

    init client

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,18 +1,18 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail({}, { message: "Invalid email" })
-  @IsNotEmpty({ message: "Email should not be empty" })
+  @IsEmail({}, { message: 'Invalid email' })
+  @IsNotEmpty({ message: 'Email should not be empty' })
   @ApiProperty()
   email: string;
 
-  @IsNotEmpty({ message: "Password should not be empty" })
+  @IsNotEmpty({ message: 'Password should not be empty' })
   @ApiProperty()
   fullName: string;
 
-  @IsNotEmpty({ message: "Password should not be empty" })
-  @MinLength(8, { message: "Password must be at least 8 characters long" })
+  @IsNotEmpty({ message: 'Password should not be empty' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @ApiProperty()
   password: string;
 }
